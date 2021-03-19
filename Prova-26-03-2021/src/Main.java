bf05810f124bc271d21bffce35ed52dcc7092e3f
Filipi Hermínio Nunes <55030682+filipinunes@users.noreply.github.com>
2021-03-19 08:56:10 -0300

    Corrigido separacao de IP e mascara

--- Prova-26-03-2021/src/Main.java
+++ Prova-26-03-2021/src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
-
-	public static void main(String[] args) {
+	public void run() {
 		Methods teste = new Methods();
-		String ip1 = teste.converte("10.0.0.1/24");
-
+		String ip1 = teste.converte("138.0.0.255/08");
+		System.out.println(ip1);
+		System.out.println(teste.identMasc(ip1));
+		System.out.println(teste.verifMasc(ip1));
+	}
+	public static void main(String[] args) {
+		new Main().run();
 	}
 
 }
--- Prova-26-03-2021/src/Methods.java
+++ Prova-26-03-2021/src/Methods.java
@@ -3,45 +3,97 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Methods {
+	//separa o ip em blocos e retorna um array
 	public List<String> getTokens(String str) {
 		List<String> tokens = new ArrayList<>();
-
-		StringTokenizer tokenizer = new StringTokenizer(str, ".");
+		
+		//Separa o IP da mascIP informada pelo usuario;
+		StringTokenizer tokenizer = new StringTokenizer(str, "/");
 		while (tokenizer.hasMoreElements()) {
 			tokens.add(tokenizer.nextToken());
 		}
+		String IP = tokens.get(0);
+		String masc = tokens.get(1);
+		//Limpa o array
+		tokens.clear();
+		
+		//Separa o IP em blocos
+		StringTokenizer tokenizerIP = new StringTokenizer(IP, ".");
+		while (tokenizerIP.hasMoreElements()) {
+			tokens.add(tokenizerIP.nextToken());
+		}
+		
+		//Adciona a mascIP no array
+		tokens.add(masc);	
 		
 		return tokens;
 	}
 	
-	public String converte(String ip) {
+	//converte o numero IP de decimal para binario
+	public String converte(String endereco) {
 		List<String> teste = new ArrayList<>();
-		teste = getTokens(ip);
-		String convertido = "";
-		for(int i=0; i<teste.size();i++) {
+		teste = getTokens(endereco);
+		
+		String mascIP = teste.get(4);
+		
+		String IPconvertido = "";
+		
+		//Percorre o array pegando somente o IP e converte pra binario
+		for(int i=0; i<4;i++) {
 			int conv = Integer.parseInt(teste.get(i));
 			int conv2 = Integer.parseInt(Integer.toBinaryString(conv));
-			if(i==teste.size()-1) {
-				convertido = convertido + String.format("%08d", conv2);
+			if(i==3) {
+				IPconvertido = IPconvertido + String.format("%08d", conv2);
 			} else {
-			convertido = convertido + String.format("%08d.", conv2);
+			IPconvertido = IPconvertido + String.format("%08d.", conv2);
 			}
 		}	
-		return convertido;
+		return IPconvertido + "/" + mascIP;
 	}
 	
-	public String identMasc(String ip) {
-		String mascara = ip.substring(0,2);
+	//Identifica a mascIP do IP
+	public String identMasc(String endereco) {
+		String mascIP = endereco.substring(0,2);
 		String classe = "";
-		if(Integer.parseInt(mascara) == 0 || Integer.parseInt(mascara) == 1) {
+		if(Integer.parseInt(mascIP) == 0 || Integer.parseInt(mascIP) == 1) {
 			classe = "A";
-		} else if (Integer.parseInt(mascara) == 10) {
+		} else if (Integer.parseInt(mascIP) == 10) {
 			classe = "B";
 		} else {
 			classe = "C";
 		}
 		return classe;
+	}
 	
+	//verifica se a mascIP informada pelo usuario eh a correta
+	public String verifMasc(String endereco) {
+		String convert;
+		String classe = "Correta.";
+		
+		convert = endereco.substring(0,2);
+		int mascIP = Integer.parseInt(convert);
+		convert = endereco.substring(endereco.length()-2,endereco.length());
+		int mascUsuario = Integer.parseInt(convert);
+		
+		if(mascIP == 0 || mascIP == 1) {
+			if(mascUsuario == 8) {
+				return classe;
+			} else {
+				return classe = "8";
+			}
+		} else if (mascIP == 10) {
+			if(mascUsuario == 16) {
+				return classe;
+			} else {
+				return classe = "16";
+			}
+		} else {
+			if(mascUsuario == 24) {
+				return classe;
+			} else {
+				return classe = "24";
+			}
+		}
 	}
 
 }
