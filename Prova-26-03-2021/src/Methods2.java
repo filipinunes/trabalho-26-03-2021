48d1ace3ef9eb609804564d09b0ea2d6ee11da8d
Filipi Hermínio Nunes <55030682+filipinunes@users.noreply.github.com>
2021-03-21 09:39:36 -0300

    Conversão de binario pra decimal

--- Prova-26-03-2021/src/Methods2.java
+++ Prova-26-03-2021/src/Methods2.java
@@ -7,18 +7,43 @@ public class Methods2 {
 	public List<String> getTokens(String str) {
 		List<String> tokens = new ArrayList<>();
 		
-		//Separa o endereco em blocos
-		StringTokenizer tokenizerIP = new StringTokenizer(str, ".");
+		String IP = str;
+		String masc = "";
+		if(str.contains("/")){
+			//Separa o IP da mascIP informada pelo usuario;
+			StringTokenizer tokenizer = new StringTokenizer(str, "/");
+			while (tokenizer.hasMoreElements()) {
+				tokens.add(tokenizer.nextToken());
+			}
+			IP = tokens.get(0);
+			masc = tokens.get(1);
+			//Limpa o array
+			tokens.clear();
+		}
+
+		
+		//Separa o IP em blocos
+		StringTokenizer tokenizerIP = new StringTokenizer(IP, ".");
 		while (tokenizerIP.hasMoreElements()) {
 			tokens.add(tokenizerIP.nextToken());
 		}
+		
+		//Adciona a mascIP no array
+		if(str.contains("/")){
+			tokens.add(masc);	
+		}
+		
 		return tokens;
 	}
 	
 	//converte o numero IP de decimal para binario
-	public List<String> converte(String endereco, String mascara) {
+	public List<String> converte(String endereco) {
 		List<String> teste = new ArrayList<>();
 		teste = getTokens(endereco);
+
+
+		String mascara = convMasc(Integer.parseInt(teste.get(4)));
+		System.out.println(mascara);
 		List<String> teste2 = new ArrayList<>();
 		teste2 = getTokens(mascara);
 
@@ -30,7 +55,7 @@ public class Methods2 {
 		//Percorre o array pegando endereco convertendo pra binario
 		for(int i=0; i<4;i++) {
 			int end = Integer.parseInt(teste.get(i));
-            int masc = Integer.parseInt(teste2.get(i));
+			int masc = Integer.parseInt(teste2.get(i));
             
             //multiplica os enderecos em decimal e joga em uma variavel string 
             int and = end & masc;
@@ -53,6 +78,29 @@ public class Methods2 {
         retorno.add(enderecoDec);
 
 		return retorno;
+	}
+
+	public String convMasc(int masc){
+		int cont = 0;
+		String endDec = "";
+		String endBin = "";
+		for(int i=0; i<4;i++){
+			for(int j=0; j<8;j++){
+				if(cont < masc){
+					endBin += "1";
+					cont ++;
+				} else {
+				endBin += "0";
+				}
+			}
+			endDec += Integer.valueOf(Integer.parseInt(endBin, 2));
+			if(i<3){
+				endDec += ".";
+			}
+			endBin = "";
+		}
+		return endDec;
+
 	}
 
 }
